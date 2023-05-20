c648d8fc46020dc0f3e9cd224fcac7a612c5b5b1
Igor <shi86@bk.ru>
2023-05-20 22:30:29 +0500

    Tasks is done

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int su_ch(int a)
+{
+    int s_ch=0;
+    while (a>0)
+    {
+        int b=a%10;
+        s_ch+=b;
+        a/=10;
+    }
+    return s_ch;
+}
+Console.WriteLine("---------------------");
+int A=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("---------------------");
+Console.WriteLine($"{A}->{su_ch(A)}");
+Console.WriteLine("---------------------");
