9eb7846d9ad230e0e227aa4a19ea736f5a3bd9bc
Igor <shi86@bk.ru>
2023-05-21 21:37:24 +0500

    Tasks is done

---
+++ Task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+void vivod_arr(int[] arr)
+ {
+     Console.Write("[");
+     Console.Write(String.Join(",",arr));
+     Console.Write("]");
+ }
+
+int[] mas=Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+vivod_arr(mas);
