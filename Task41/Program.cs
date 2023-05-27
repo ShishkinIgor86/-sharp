e3eda1f08b418c58d82e9f4e4727a3bdd3960ef8
Igor <shi86@bk.ru>
2023-05-27 12:46:16 +0500

    Tasks is done

---
+++ Task41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int count_pos(int[] array)
+{
+    int posit=0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+            posit+=1;
+        }
+    }
+    return posit;
+}
+
+void vivod_arr(int[] arr)
+ {
+    Console.Write(String.Join(",",arr));
+ }
+
+Console.WriteLine("Введите последовательность чисел \n(числа вводятся в одну строку через запятую, Enter окончание ввода):");
+int[] array = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+vivod_arr(array);
+Console.WriteLine($"->{count_pos(array)}");
---
+++ Task43/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void pp(double[] coord)
+{
+    if (coord[0]==coord[2])
+    {
+        Console.WriteLine("Прямые параллельны, точек пересения нет");
+    }
+    else
+    {
+        double x=(coord[3]-coord[1])/(coord[0]-coord[2]);
+        double y=coord[0]*x+coord[1];
+        Console.WriteLine($"({x},{y})");
+    }
+}
+Console.WriteLine("Введите коэффициенты k1,b1,k2,b2 через запятую: ");
+double[] coord=Console.ReadLine().Split(',').Select(double.Parse).ToArray();
+Console.Write($"b1={coord[1]},k1={coord[0]},b2={coord[3]},k2={coord[2]}->");
+pp(coord);
